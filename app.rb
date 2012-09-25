d87eb7b85c18d8c694e66d4324fce91afcf04b03
emadb <emanuele@codiceplastico.com>
2012-09-25 18:13:00 +0200

    basic operations

--- app.rb
+++ app.rb
@@ -4,7 +4,24 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require 'model'
 
+before do
+  content_type :json
+end
 
-get '/' do
+get '/todos' do
 	Todo.all.to_json
 end
+
+get '/todo/:id' do
+  todo = Todo.find(params[:id])
+  todo.to_json
+end
+
+post '/todo/' do
+  todo = Todo.new
+  todo.description = params[:description]
+  t = todo.save
+  puts todo._id
+  status, body = 201, {:url => "/todo/#{todo._id}"}.to_json
+end
+
